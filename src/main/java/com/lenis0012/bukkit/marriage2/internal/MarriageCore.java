ae6823f308f393ca193ef63f06f6be7857c1ecc8
IAmSoccer <therealsoccerball500@gmail.com>
2022-10-06 22:23:12 -0500

    Removes Updater

--- src/main/java/com/lenis0012/bukkit/marriage2/internal/MarriageCore.java
+++ src/main/java/com/lenis0012/bukkit/marriage2/internal/MarriageCore.java
@@ -15,9 +15,6 @@ import com.lenis0012.bukkit.marriage2.internal.data.MarriagePlayer;
 import com.lenis0012.bukkit.marriage2.listeners.*;
 import com.lenis0012.bukkit.marriage2.misc.ListQuery;
 import com.lenis0012.pluginutils.modules.configuration.ConfigurationModule;
-import com.lenis0012.updater.api.ReleaseType;
-import com.lenis0012.updater.api.Updater;
-import com.lenis0012.updater.api.UpdaterFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -31,7 +28,6 @@ import java.util.logging.Level;
 public class MarriageCore extends MarriageBase {
     private final Map<UUID, MarriagePlayer> players = Collections.synchronizedMap(new HashMap<UUID, MarriagePlayer>());
     private DataManager dataManager;
-    private Updater updater;
     private Dependencies dependencies;
 
     public MarriageCore(MarriagePlugin plugin) {
@@ -90,7 +86,6 @@ public class MarriageCore extends MarriageBase {
         register(new ChatListener(this));
         register(new DatabaseListener(this));
         register(new KissListener(this));
-        register(new UpdateListener(this));
     }
 
     private void hookPlotSquared() {
@@ -134,17 +129,10 @@ public class MarriageCore extends MarriageBase {
                 CommandReload.class,
                 CommandSeen.class,
                 CommandSethome.class,
-                CommandTeleport.class,
-                CommandUpdate.class
+                CommandTeleport.class
         );
     }
 
-    @Register(name = "updater", type = Type.ENABLE, priority = 9)
-    public void loadUpdater() {
-        UpdaterFactory factory = new UpdaterFactory(plugin, "com.lenis0012.bukkit.marriage2.libs.updater");
-        this.updater = factory.newUpdater(plugin.getPluginFile(), Settings.ENABLE_UPDATE_CHECKER.value());
-        updater.setChannel(ReleaseType.valueOf(Settings.UPDATER_CHANNEL.value().toUpperCase()));
-    }
 
     @Register(name = "converter", type = Register.Type.ENABLE, priority = 10)
     public void loadConverter() {
@@ -222,10 +210,6 @@ public class MarriageCore extends MarriageBase {
 
     public DataManager getDataManager() {
         return dataManager;
-    }
-
-    public Updater getUpdater() {
-        return updater;
     }
 
     public void removeMarriage(final MData mdata) {
